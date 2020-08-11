5f7a751d53de8e5038c551efb20f82e33e916ccc
Field Employee <fieldemployee1@Fields-MacBook-Pro-4.local>
2020-08-10 20:12:04 -0400

    setting up cocoapods and added alamo fire

--- PokemonBlue/Network/Manger.swift
+++ PokemonBlue/Network/Manger.swift
@@ -6,7 +6,7 @@
 //  Copyright © 2020 JediWattson. All rights reserved.
 //
 
-import UIKit
+import Alamofire
 
 typealias ListHandler = (Result<PokemonList, NetworkError>) -> ()
 typealias imgHandler = (Result<UIImage, NetworkError>) -> ()
@@ -27,29 +27,14 @@ class NetworkManager {
 
 extension NetworkManager {
     func fetchPokemonList(_ url: String, completion: @escaping ListHandler){
-        guard let url = URL(string: url) else {
-            completion(.failure(.badURL))
-            return
-        }
-        self.session.dataTask(with: url) { (data, res, err) in
-            if let err = err {
-                completion(.failure(.serverError(err.localizedDescription)))
+        AF.request(url).validate().responseDecodable(of: PokemonList.self ){ response in
+            guard let list = response.value else {
+                guard let errorString = response.error?.localizedDescription else {return}
+                completion(.failure(.serverError(errorString)))
                 return
             }
-            
-            guard let data = data else {
-                completion(.failure(.badData))
-               return
-            }
-
-            do {
-                let list = try self.decoder.decode(PokemonList.self, from: data)
-                completion(.success(list))
-            } catch {
-                completion(.failure(.decodeError))
-            }
-        }.resume()
-        
+            completion(.success(list))
+        }
     }
     
     func decodePokemon(data: Data, completion: @escaping PokemonTrainer){
@@ -73,30 +58,33 @@ extension NetworkManager {
         }
     }
     
-    func fetchPokemon(_ pokemonURL: String, completion: @escaping PokemonTrainer) {
-        if let data = cache.get(url: pokemonURL){
+    func fetchPokemon(_ url: String, completion: @escaping PokemonTrainer) {
+        if let data = cache.get(url: url){
             self.decodePokemon(data: data, completion: completion)
             return
         }
-        
-        guard let url = URL(string: pokemonURL) else {
-            completion(.failure(.badURL))
-            return
-        }
-        
-        self.session.dataTask(with: url) { (data, res, err) in
-            if let err = err {
-                completion(.failure(.serverError(err.localizedDescription)))
+        AF.request(url).validate().responseDecodable(of: Pokemon.self ){ response in
+            guard var pokemon = response.value else {
+                guard let errorString = response.error?.localizedDescription else {return}
+                completion(.failure(.serverError(errorString)))
                 return
             }
-            
-            guard let data = data else {
-                completion(.failure(.badData))
-               return
+            guard let imageURL = pokemon.sprites.frontDefault else {
+                completion(.failure(.badURL))
+                return
             }
-            self.cache.set(data: data, url: pokemonURL)
-            self.decodePokemon(data: data, completion: completion)
-        }.resume()
+            self.fetchSprite(imageURL){result in
+                switch result {
+                case .success(let image):
+                    pokemon.image = image
+                    completion(.success(pokemon))
+                case .failure(let error):
+                    completion(.failure(error))
+                }
+            }
+
+        }
+        
     }
     
     func setImage(data: Data, completion: @escaping imgHandler){
@@ -107,33 +95,26 @@ extension NetworkManager {
         completion(.success(image))
     }
     
-    func fetchSprite(_ imgURL: String, completion: @escaping imgHandler) {
+    func fetchSprite(_ url: String, completion: @escaping imgHandler) {
         
-        if let data = cache.get(url: imgURL) {
+        if let data = cache.get(url: url) {
             self.setImage(data: data, completion: completion)
             return
         }
         
-        guard let url = URL(string: imgURL) else {
-            completion(.failure(.badURL))
-            return
-        }
-        
-        self.session.dataTask(with: url) { (data, res, err) in
-            
-            if let err = err {
+        AF.request(url).validate().response{
+            response in
+            if let err = response.error {
                 completion(.failure(.serverError(err.localizedDescription)))
                 return
             }
-            
-            guard let data = data else {
+            guard let data = response.data else {
                 completion(.failure(.badData))
-               return
+                return
             }
-            
-            self.cache.set(data: data, url: imgURL)
+            self.cache.set(data: data, url: url)
             self.setImage(data: data, completion: completion)
-        }.resume()
+        }
     }
-    
 }
+
